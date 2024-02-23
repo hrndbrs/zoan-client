de0000f5e578934e685fc3e3f6f19ee84a21900c
Hernando <hrndbrs@gmail.com>
2024-02-23 15:04:31 +0700

    chore: configured tailwinds, adding font Barlow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,54 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "natural-1": "#FEFEFE",
+        "natural-2": "#E8E8E8",
+        "natural-3": "#B9B9B9",
+        "natural-4": "#979797",
+        "natural-5": "#686868",
+        "natural-6": "#4A4A4A",
+        "natural-7": "#1D1D1D",
+        "natural-8": "#1A1A1A",
+        "z-teal": "#61929E",
+        "z-indigo": "#BFC5E2",
+      },
+      fontFamily: {
+        barlow: ["Barlow", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
