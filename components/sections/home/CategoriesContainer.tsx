b3a61a0d7672bf95432579af83360f64d86bd7e5
hrndbrs <hrndbrs@gmail.com>
2024-03-23 00:48:35 +0700

    test

--- components/sections/home/CategoriesContainer.tsx
+++ components/sections/home/CategoriesContainer.tsx
@@ -9,6 +9,8 @@ export default async function CategoriesContainer() {
   if (res) {
     const categories = formatCategoryList(res);
 
+    console.log(process.env, "=======================");
+
     return (
       <section id="categories" className="px-5 py-[7.5rem] xl:h-[110rem] md:h-[144rem]">
         <div className="inner-container flex-col !justify-start flex-wrap h-full gap-8">
