470ad2ace4a6253840ad91fd843d38ea931b1e40
hrndbrs <hrndbrs@gmail.com>
2024-03-22 22:37:17 +0700

    fix: add async rewrites to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api",
+        destination: `${process.env.NEXT_APP_BASE_URL}/:path*`,
+      },
+    ];
+  },
   env: {
     BACKEND_BASE_URL: process.env.NEXT_APP_IMAGE_URL,
     NEXT_APP_API_URL: process.env.NEXT_APP_API_URL
