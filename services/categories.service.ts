987d0dc4236c455b93434cd360fdbc0774fd8a42
Juan Daniel <juan.daniel@enigmacamp.com>
2024-03-24 00:41:25 +0700

    add: no store cache

--- services/categories.service.ts
+++ services/categories.service.ts
@@ -2,10 +2,12 @@
 
 import api from "@/config/axios";
 import { Categories } from "@/lib/types/categories.type";
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function getProductCategories() {
+  noStore();
   try {
     const { data } = await api.get<Categories>("/categories?populate[0]=image");
     return data.data;
-  } catch (error) {}
+  } catch (error) { }
 }
